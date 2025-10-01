b66ad36f0d60f6337db4da3933b3f9e609da55bf
omerwarth <139028060+omerwarth@users.noreply.github.com>
2025-10-01 16:32:37 -0400

    Fixed build warning

--- capgemini-hospitality/next.config.ts
+++ capgemini-hospitality/next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true
-  }
+  },
+  outputFileTracingRoot: path.join(process.cwd())
 };
 
 export default nextConfig;
